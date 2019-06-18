573e9754cdb81690ef8b5e47623808321a4c946f
babos <babos77@gmail.com>
2019-06-18 21:04:28 +0300

    add pytest mark

--- test_mark.py
+++ test_mark.py
@@ -14,16 +14,14 @@ def browser():
     browser.quit()
 
 
-@pytest.fixture(autouse=True)
-def prepare_data():
-    print("preparing some critical data...")
-
-
 class TestMainPAge1:
+
+    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
 
+    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
